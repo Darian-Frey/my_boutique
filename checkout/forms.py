7d783ba234ecdc33a61d14f0d63bda0dbf101f17
Darian-Frey <shane.hartley06@gmail.com>
2021-08-05 12:47:05 +0000

    Added profile form, views and updated templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,4 +1,3 @@
- 
 from django import forms
 from .models import Order
 
@@ -38,4 +37,3 @@ class OrderForm(forms.ModelForm):
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
-  
